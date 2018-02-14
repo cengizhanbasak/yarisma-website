1561f9c7aafb87dc3e95d9650f46bd0b839a50a2
duck2 <sikerbela@riseup.net>
2018-02-14 15:31:51 +0300

    current year on footer

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 app.use( express.static(__dirname + '/public' , { maxage: '1d' }) );
 
 
-
 app.get('/',function(req,res){
   res.setHeader("Cache-Control", "public, max-age=2592000");
   res.render('./src/index.ejs',{active:'yarisma'});
