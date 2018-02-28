0c637c6300677db5061b3d9c4c3320245ba4cc15
Cengizhan Başak <cengizhanbasakk@gmail.com>
2018-02-28 20:05:05 +0300

    Register form added

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/',function(req,res){
   res.render('./src/index.ejs',{active:'yarisma'});
 });
 
+app.get('/kayit',function(req,res){
+  res.render('./src/kayit.ejs',{active:'kayit'})
+});
+
 app.get('/sss',function(req,res){
   res.render('./src/sss.ejs',{active:'sss'})
 });
